0c90437bafcb881e54c07abb440531e1f329578d
tutkuince <tutku.ince@outlook.com>
2019-02-08 21:33:22 +0300

    eventListeners

---
+++ WebContent/js/async/app.js
@@ -0,0 +1,21 @@
+// async, await
+
+const amount = document.getElementById("amount");
+const firstCurrency = document.getElementById("firstCurrency");
+const secondCurrency = document.getElementById("secondCurrency");
+
+eventListeners();
+
+function eventListeners() {
+    amount.addEventListener("input", exchangeCurrency);
+    firstCurrency.onchange = function () {
+
+    };
+    secondCurrency.onchange = function () {
+
+    };
+}
+
+function exchangeCurrency() {
+
+}
