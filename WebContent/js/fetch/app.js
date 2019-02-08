26001720fecfe8dc01e814d1fc2fa6a2705668b1
tutkuince <tutku.ince@outlook.com>
2019-02-08 21:18:51 +0300

    changeFirstCurrency, changeSecondCurrency, changeAmount

--- WebContent/js/fetch/app.js
+++ WebContent/js/fetch/app.js
@@ -9,13 +9,18 @@ eventListeners();
 function eventListeners() {
     amount.addEventListener("input", exchangeCurrency);
     firstCurrency.onchange = function () {
-
+        currency.changeFirstCurrency(firstCurrency.value);
     };
     secondCurrency.onchange = function () {
-
+        currency.changeSecondCurrency(secondCurrency.value);
     };
 }
 
-function exchangeCurrency() {
+const currency = new Currency(firstCurrency.value, secondCurrency.value);
 
+function exchangeCurrency() {
+    currency.changeAmount(amount.value);
+    currency.exchange()
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 }
