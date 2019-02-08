a83f6dd2d5b5ff3cae3edbf5dd9647ebb718aafd
tutkuince <tutku.ince@outlook.com>
2019-02-08 20:31:12 +0300

    app

--- WebContent/js/prototype/app.js
+++ WebContent/js/prototype/app.js
@@ -1,1 +1,32 @@
 // Prototype, Ajax, Callback
+
+// Select elements
+const amount = document.getElementById("amount");
+const firstCurrency = document.getElementById("firstCurrency");
+const secondCurrency = document.getElementById("secondCurrency");
+
+eventListeners();
+
+function eventListeners() {
+    amount.addEventListener("input", exchangeCurrency);
+
+    firstCurrency.onchange = function () {
+        currency.changeFirstCurrency(firstCurrency.value);
+    };
+
+    secondCurrency.onchange = function () {
+        currency.changeSecondCurrency(secondCurrency.value);
+    }
+}
+
+const currency = new Currency(firstCurrency.value, secondCurrency.value);
+
+function exchangeCurrency() {
+    currency.changeAmount(amount.value);
+    currency.exchange(function (err, response) {
+        if (err === null)
+            console.log(response);
+        else
+            console.log(err);
+    });
+}
