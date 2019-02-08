8fee7801268e5012ae5b9c8754a84d29de8ce9ab
tutkuince <tutku.ince@outlook.com>
2019-02-08 21:26:20 +0300

    initialized UI object. invoked displayResult and changeUI

--- WebContent/js/fetch/app.js
+++ WebContent/js/fetch/app.js
@@ -10,17 +10,20 @@ function eventListeners() {
     amount.addEventListener("input", exchangeCurrency);
     firstCurrency.onchange = function () {
         currency.changeFirstCurrency(firstCurrency.value);
+        ui.changeUI(firstCurrency);
     };
     secondCurrency.onchange = function () {
         currency.changeSecondCurrency(secondCurrency.value);
+        ui.changeUI(secondCurrency);
     };
 }
 
 const currency = new Currency(firstCurrency.value, secondCurrency.value);
+const ui = new UI();
 
 function exchangeCurrency() {
     currency.changeAmount(amount.value);
     currency.exchange()
-        .then(data => console.log(data))
+        .then(data => ui.displayResult(data))
         .catch(err => console.log(err));
 }
